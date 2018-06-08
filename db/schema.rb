8713bf537b82f3f29bb15d3289a80384da2844c4
Bhagavathi Chirra <bhagavathi.chirra@gmail.com>
2018-06-08 16:19:35 +1000

    seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_08_045225) do
+ActiveRecord::Schema.define(version: 2018_06_08_054429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.text "name"
+    t.text "image"
+    t.text "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "title"
+    t.text "image"
+    t.integer "city_id"
+    t.integer "user_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.text "username"

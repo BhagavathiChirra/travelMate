b08ab68e9b32b8c38ee153bd6bd909b6a98e4f90
Bhagavathi Chirra <bhagavathi.chirra@gmail.com>
2018-06-12 17:54:58 +1000

    posts.js refactored

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -20,23 +20,8 @@ $( document ).ready(function() {
       })
       .done(function (data) {
         $('#txt_area_comments').val('');
-        var commentsContent = `Posted By: ${data.user.username}<br>Comment: ${data.content}<a class='delete_comment' href='javascript:void(0)' id='${data.id}'>Delete</a><br>`;
-        $p = $('<p>').html(commentsContent);
-        $p.appendTo('#comments_div');
-        $('.delete_comment').on('click', function(){
-          var idAttr = $(this).attr('id');
-          const url_destroy = `http://localhost:3000/comments/${idAttr}`;
-          $.ajax({
-            url: url_destroy,
-            type: 'DELETE'
-          })
-          .done(function(){
-            $('#comments_div').html("");
-            getCommentsJson();
-          })
-          .fail(errorHandler);
-          // console.log(idAttr);
-        });
+        const comment = data;
+        displayComment(comment);
       })
       .fail(errorHandler);
     });
@@ -58,37 +43,38 @@ const getCommentsJson = function(){
 const getComments = data => {
   for(i=0; i< data.length; i++){
     const comment = data[i];
-    var $display_delete;
-    if(userId === data[i].user.id){
-
-      $display_delete = $(`<a class="delete_comment">Delete</a>`);
-      $display_delete.on('click', function(){
-        const url_destroy = `http://localhost:3000/comments/${ comment.id }`;
-        // console.log('url', url_destroy);
-        $.ajax({
-          url: url_destroy,
-          type: 'DELETE'
-        })
-        .done(function(){
-          // $('#comments_div').html(""); // getCommentsJson();
-          $(`#comment_${ comment.id }`).remove();
-        })
-        .fail(errorHandler);
-      });
-    }
-    else {
-      $display_delete = "";
-    }
-    var commentsContent = `Posted By: ${data[i].user.username}<br>Comment: ${data[i].content}`;
-
-    $comment = $(`<div id="comment_${ comment.id }">`).html(`<p>${ commentsContent }</p>`);
-    $comment.append($display_delete);
-    $comment.appendTo('#comments_div');
-    // $p = $('<p>').html(commentsContent);
-    // $p.appendTo('#comments_div');
-
+    displayComment(comment);
   }
 };
 const errorHandler = xhr => {
   console.warn('Error with comments request: ', xhr.responseText, xhr);
+};
+
+const displayComment = comment => {
+  var $display_delete;
+  if(userId === comment.user.id){
+
+    $display_delete = $(`<a class="delete_comment">Delete</a>`);
+    $display_delete.on('click', function(){
+      const url_destroy = `http://localhost:3000/comments/${ comment.id }`;
+      // console.log('url', url_destroy);
+      $.ajax({
+        url: url_destroy,
+        type: 'DELETE'
+      })
+      .done(function(){
+        // $('#comments_div').html(""); // getCommentsJson();
+        $(`#comment_${ comment.id }`).remove();
+      })
+      .fail(errorHandler);
+    });
+  }
+  else {
+    $display_delete = "";
+  }
+  var commentsContent = `Posted By: ${comment.user.username}<br>Comment: ${comment.content}`;
+
+  $comment = $(`<div id="comment_${ comment.id }">`).html(`<p>${ commentsContent }</p>`);
+  $comment.append($display_delete);
+  $comment.appendTo('#comments_div');
 };
